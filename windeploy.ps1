b72881347c4e0e838972257ed65e79756a0e7517
picanl <36163190+picanl@users.noreply.github.com>
2023-09-06 11:56:34 +0200

    Update windeploy.ps1

--- windeploy.ps1
+++ windeploy.ps1
@@ -9,7 +9,7 @@ if ((Get-MyComputerModel) -match 'Virtual') {
 
 #Make sure I have the latest OSD Content
 Write-Host  -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
-Install-Module OSD -Force
+#Install-Module OSD -Force
 
 Write-Host  -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
 #Import-Module OSD -Force
