24bfa04979c8780b547be13003ebed8430779778
picanl <36163190+picanl@users.noreply.github.com>
2023-09-06 14:26:35 +0200

    Update windeploy.ps1

--- windeploy.ps1
+++ windeploy.ps1
@@ -7,52 +7,55 @@ if ((Get-MyComputerModel) -match 'Virtual') {
     Set-DisRes 1600
 }
 # Define a function to display the menu
-function Show-Menu {
-    Clear-Host
-    Write-Host "==================== Menu ===================="
-    Write-Host "1. Carmel Windows 10 64bit Education (laatste Versie)"
-    Write-Host "2. Carmel Windows 11 64bit Education (laatste Versie)"
-    Write-Host "3. Reboot"
-    Write-Host "4. Exit"
-    Write-Host "=============================================="
+
+# Display the menu
+Clear-Host
+Write-Host "==================== Carmel Intune Deployment Tool ======================="
+Write-Host ""
+Write-Host ""
+Write-Host "==================== Menu ===================="
+Write-Host "1. Carmel Windows 10 64bit Education (laatste Versie)"
+Write-Host "2. Carmel Windows 11 64bit Education (laatste Versie)"
+Write-Host "3. Reboot"
+Write-Host "=============================================="
+
+# Prompt for user input
+$choice = Read-Host "Select an option (1-4)"
+
+# Use a switch statement to perform actions based on user choice
+switch ($choice) {
+    "1" {
+        # Add code for Option 1 here
+        Write-Host "You selected Option 1"
+        Start-OSDCloud -OSLanguage nl-nl -OSName 'Windows 10 22H2 x64' -OSEdition Education -ZTI
+
+        # Exit the script
+        #exit
+    }
+    "2" {
+        # Add code for Option 2 here
+        Write-Host "You selected Option 2"
+        Start-OSDCloud -OSLanguage nl-nl -OSName 'Windows 11 22H2 x64' -OSEdition Education -ZTI
+
+        # Exit the script
+        #exit
+    }
+    "3" {
+        # Add code for Option 3 here
+        Write-Host "You selected Option 3"
+        wpeutil reboot
+
+        # Exit the script
+        #exit
+    }
+    default {
+        Write-Host "Invalid choice. Please select a valid option (1-4)."
+    }
 }
 
-# Main menu loop
-do {
-    Show-Menu
 
-    # Prompt for user input
-    $choice = Read-Host "Select an option (1-4)"
 
-    # Use a switch statement to perform actions based on user choice
-    switch ($choice) {
-        "1" {
-            # Add code for Option 1 here
-            Write-Host "You selected Option 1"
-            Start-OSDCloud -OSLanguage nl-nl -OSName 'Windows 10 22H2 x64' -OSEdition Education -ZTI
-            break
-        }
-        "2" {
-            # Add code for Option 2 here
-            Write-Host "You selected Option 2"
-            Start-OSDCloud -OSLanguage nl-nl -OSName 'Windows 11 22H2 x64' -OSEdition Education -ZTI
-            break
-        }
-        "3" {
-            # Add code for Option 3 here
-            Write-Host "You selected Option 3"
-            wpeutil reboot
-        }
-        "4" {
-            Write-Host "Exiting..."
-            wpeutil reboot
-            break # Exit the loop and the script
-        }
-        default {
-            Write-Host "Invalid choice. Please select a valid option (1-4)."
-        }
-    }
-} while ($true)
+
 
 #Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
 Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
